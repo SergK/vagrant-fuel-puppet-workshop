174a8f4b2fd6d006361412a82e39fd9cf213c3c9
Sergey Kulanov <skulanov@mirantis.com>
2014-12-11 11:46:04 +0200

    add hostname to /etc/hosts

--- virtualbox/bootstrap.sh
+++ virtualbox/bootstrap.sh
@@ -4,28 +4,26 @@ sudo tee -a /etc/hosts <<EOF
 
 192.168.50.10 pxetool.test.local
 # ensure from site.pp that you have jenkins master on this node
-192.168.50.11 slave-01.test.local jenkins-master.test.local jenkins-product.test.local
-192.168.50.12 slave-02.test.local
-192.168.50.13 slave-03.test.local
-192.168.50.14 slave-04.test.local
-192.168.50.15 slave-05.test.local
-192.168.50.16 slave-06.test.local
-192.168.50.17 slave-07.test.local
-192.168.50.18 slave-08.test.local
-192.168.50.19 slave-09.test.local
-192.168.50.20 slave-10.test.local
-192.168.50.21 slave-11.test.local
-192.168.50.22 slave-12.test.local
-192.168.50.23 slave-13.test.local
-192.168.50.24 slave-14.test.local
-192.168.50.25 slave-15.test.local
-192.168.50.26 slave-16.test.local
-192.168.50.27 slave-17.test.local
-192.168.50.28 slave-18.test.local
-192.168.50.29 slave-19.test.local
-192.168.50.30 slave-20.test.local
-
-172.18.198.42 osci-mirror-kha.kha.mirantis.net
+192.168.50.11 slave-01.test.local jenkins-master.test.local jenkins-product.test.local slave-01
+192.168.50.12 slave-02.test.local slave-02
+192.168.50.13 slave-03.test.local slave-03
+192.168.50.14 slave-04.test.local slave-04
+192.168.50.15 slave-05.test.local slave-05
+192.168.50.16 slave-06.test.local slave-06
+192.168.50.17 slave-07.test.local slave-07
+192.168.50.18 slave-08.test.local slave-08
+192.168.50.19 slave-09.test.local slave-09
+192.168.50.20 slave-10.test.local slave-10
+192.168.50.21 slave-11.test.local slave-11
+192.168.50.22 slave-12.test.local slave-12
+192.168.50.23 slave-13.test.local slave-13
+192.168.50.24 slave-14.test.local slave-14
+192.168.50.25 slave-15.test.local slave-15
+192.168.50.26 slave-16.test.local slave-16
+192.168.50.27 slave-17.test.local slave-17
+192.168.50.28 slave-18.test.local slave-18
+192.168.50.29 slave-19.test.local slave-19
+192.168.50.30 slave-20.test.local slave-20
 
 EOF
 
