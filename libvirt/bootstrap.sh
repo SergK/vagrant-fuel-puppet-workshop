94a55c78e664c23a51c23381f0ba3ad41cf81085
Sergey Kulanov <skulanov@mirantis.com>
2014-12-19 21:45:39 +0200

    Some refactoring for bootstrap

--- libvirt/bootstrap.sh
+++ libvirt/bootstrap.sh
@@ -35,9 +35,6 @@ sudo apt-get update && sudo apt-get upgrade -y
 
 sudo chown vagrant:vagrant -R /home/vagrant/
 
-sudo mkdir -p /var/lib/hiera
-sudo chown root:root -R /var/lib/hiera
-
 # install packages
 sudo apt-get install -y puppet
 
@@ -46,9 +43,14 @@ sudo mkdir -p /etc/puppet/bin
 # add server to config
 sed -i '/ssldir/a \server=pxetool.test.local' /etc/puppet/puppet.conf
 
-if ! grep "^slave" /etc/hostname; then
+if grep "^pxetool" /etc/hostname; then
   # install git
   sudo apt-get install -y git
+
+  # create dir for
+  sudo mkdir -p /var/lib/hiera
+  sudo chown root:root -R /var/lib/hiera
+
   # install puppet master
   sudo /etc/puppet/bin/install_puppet_master.sh
 else
